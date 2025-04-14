edab4d0c0d0c8fd8536f12aa3af6a2ea9c3ea5ef
Norbert Klessen <norbert.klessen@planteon.pl>
2025-04-14 11:12:12 +0200

    init commit

---
+++ planteonAiSpring/src/main/java/com/example/planteonAi/PlanteonAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.planteonAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanteonAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanteonAiApplication.class, args);
+	}
+
+}
---
+++ planteonAiSpring/src/test/java/com/example/planteonAi/PlanteonAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planteonAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanteonAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
